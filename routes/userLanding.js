4315c0ae44eda0db7710e912c14f973186a39b7d
calebyxu <calebxu1234@gmail.com>
2025-07-21 13:17:42 -0700

    sdfsdf

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -15,6 +15,7 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
     res.render("userLanding");
     console.log(req.body);
+    console.log("hello")
 });
 
 router.put("/", (req, res) => {
