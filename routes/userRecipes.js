f255c9d7dec20d2a6885139436480622398a08a8
calebyxu <calebxu1234@gmail.com>
2025-07-21 12:52:01 -0700

    bug fixing

--- routes/userRecipes.js
+++ routes/userRecipes.js
@@ -38,6 +38,7 @@ async function getRecipe() {
 
 router.get("/", async (req, res) => {
     res.render("wip");
+    console.log(req.body);
     // console.log("userRecipes");
 
     // var data = await getRecipe();
