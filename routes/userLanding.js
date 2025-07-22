38f6a65119fff17218c786e3b370cb3da3914990
calebyxu <calebxu1234@gmail.com>
2025-07-22 12:06:28 -0700

    testing to grab name from json

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -14,7 +14,7 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
     res.render("userLanding");
-    console.log(req.body);
+    console.log(req.body['name']);
 });
 
 router.put("/", (req, res) => {
