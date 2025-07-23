565930fbceadb01431b5ecfa3c3bae3891f9b045
calebyxu <calebxu1234@gmail.com>
2025-07-23 13:29:32 -0700

    dfgfdgfdg

---
+++ public/js/otherStuff.js
@@ -0,0 +1,13 @@
+// try {
+        //     const response = await fetch("https://umai-site-fc33abfe2d2d.herokuapp.com/home", {
+        //         method: "POST",
+        //         mode: 'cors',
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify(responsePayload)
+        //     });
+        //     console.log(response)
+        // } catch (e) {
+        //     console.error(e);
+        // }
