c050cb971f985bce2f17cff6782ada25b6ea8f8f
calebyxu <calebxu1234@gmail.com>
2025-07-22 18:40:42 -0700

    bug fixing

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    res.redirect("userLanding", { name: req.body['name']});
+    res.render("userLanding", { name: req.body['name']});
 });
 
 router.put("/", (req, res) => {
