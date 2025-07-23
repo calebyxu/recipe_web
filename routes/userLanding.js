dc32bd49b9f85fa8066321bcdbe31055e05b5419
calebyxu <calebxu1234@gmail.com>
2025-07-23 14:05:48 -0700

    vfcxasdsada

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    res.render("userLanding", { name: ""});
+    res.render("userLanding", { name: req.body['pog']});
 });
 
 router.put("/", (req, res) => {
