5ddbdcf58ed49a9ad709badc3e05d26fbfedad0b
calebyxu <calebxu1234@gmail.com>
2025-07-23 11:59:06 -0700

    bug fixing

--- routes/userLanding.js
+++ routes/userLanding.js
@@ -8,7 +8,7 @@ router.use(express.static(path.join(__dirname, '..', 'public')));
 
 router.get("/", (req, res) => {
     console.log("userlanding");
-    res.render("userLanding", { name: "" });
+    res.render("userLanding", { name: "hello" });
     console.log(path.join(__dirname, '..', 'public'));
 });
 
